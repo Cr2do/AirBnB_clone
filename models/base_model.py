b97f7f61ba3480108fa7bcd1b413dae8b62b7426
Cr2do <credoay@gmail.com>
2022-08-05 00:34:00 +0100

    Task on BaseModel done

--- models/base_model.py
+++ models/base_model.py
@@ -7,27 +7,26 @@ from datetime import datetime
 import uuid
 import json
 
-
 class BaseModel:
     """
     BaseModel Class
     """
 
     def __init__(self):
-        self.id = uuid.uuid4()
-        self.created_at = datetime().today()
-        self.updated_at = datetime().today()
-
-    def __str__():
-        pass
+        self.id = uuid.uuid4().__str__()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def save(self):
-        self.updated_at = datetime().now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return str("[" + self.__class__.__name__ + "] (" + self.id + ") " + str(self.__dict__))
 
     def to_dict(self):
-        valueToReturn = {}
-        valueToReturn.pop(self.__dict__)
-        valueToReturn.pop(self.__class__())
-        valueToReturn.pop(json.dump(self.created_at, fp))
-        valueToReturn.pop(json.dump(self.updated_at, fp))
-        return valueToReturn
+        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.id = str(self.id)
+        self.__dict__["__class__"] = str(self.__class__)
+        value_to_return = self.__dict__
+        return value_to_return
